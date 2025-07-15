7f2c47577050604476e11da9e135415211bf507d
Israa <e.alshaikh.dev@gmail.com>
2025-07-15 10:04:26 +0300

    New Style

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
 export class RolesComponent {
-
+// string, number, array, null, undefined, object, boolean, date
+  fullname = "Esraa Alshaikh"
+  firstname = "Maria"
+  lastname = "Khalil"
 }
